0fc9ed32a38ae18d3181b3a8c265a226c6f8738c
bhanuprakash2 <matthew00220022@gmail.com>
2021-01-27 20:01:30 +0530

    starter files added

---
+++ client/src/components/streams/index.js
@@ -0,0 +1,13 @@
+import StreamCreate from './StreamCreate';
+import StreamDelete from './StreamDelete';
+import StreamEdit from './StreamEdit';
+import StreamList from './StreamList';
+import StreamShow from './StreamShow';
+
+export {
+    StreamCreate,
+    StreamDelete,
+    StreamEdit,
+    StreamShow,
+    StreamList
+}; 
